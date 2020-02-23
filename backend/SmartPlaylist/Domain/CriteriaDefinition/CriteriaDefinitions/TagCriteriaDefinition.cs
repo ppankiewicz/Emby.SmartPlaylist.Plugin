52fde16003725ffb00c41b1bab0c0edfd42682b4
ppankiewicz <23923+ppankiewicz@users.noreply.github.com>
2020-02-23 19:51:51 +0100

    feat: filter by Tags

---
+++ backend/SmartPlaylist/Domain/CriteriaDefinition/CriteriaDefinitions/TagCriteriaDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using SmartPlaylist.Domain.Rule;
+using SmartPlaylist.Domain.Values;
+
+namespace SmartPlaylist.Domain.CriteriaDefinition.CriteriaDefinitions
+{
+    public class TagCriteriaDefinition : CriteriaDefinition
+    {
+        public override string Name => "Tag";
+        public override CriteriaDefinitionType Type => StringDefinitionType.Instance;
+
+        public override Value GetValue(UserItem item)
+        {
+            return ArrayValue<StringValue>.Create(item.Item.Tags?.Select(StringValue.Create).ToArray());
+        }
+    }
+}
