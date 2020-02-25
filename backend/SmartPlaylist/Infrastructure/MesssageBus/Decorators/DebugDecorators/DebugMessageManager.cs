ba4d0077de2aea78dec3922717a79e6fc7151ae0
ppankiewicz <23923+ppankiewicz@users.noreply.github.com>
2020-02-25 23:56:57 +0100

    chore: add debug messageHandler decorators

---
+++ backend/SmartPlaylist/Infrastructure/MesssageBus/Decorators/DebugDecorators/DebugMessageManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Controller.Session;
+using MediaBrowser.Model.Session;
+
+namespace SmartPlaylist.Infrastructure.MesssageBus.Decorators.DebugDecorators
+{
+    public class DebugMessageManager
+    {
+        private readonly ISessionManager _sessionManager;
+
+        public DebugMessageManager(ISessionManager sessionManager)
+        {
+            _sessionManager = sessionManager;
+        }
+
+        public async Task ShowAsync(string text)
+        {
+            var command = new MessageCommand
+            {
+                Header = "head",
+                TimeoutMs = (long?) TimeSpan.FromSeconds(10).TotalMilliseconds,
+                Text = text
+            };
+
+            foreach (var session in _sessionManager.Sessions.ToList())
+                await ShowMessage(session, command).ConfigureAwait(false);
+        }
+
+        private async Task ShowMessage(SessionInfo session, MessageCommand command)
+        {
+            await _sessionManager.SendMessageCommand(session.Id, session.Id, command, CancellationToken.None)
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ backend/SmartPlaylist/Infrastructure/MesssageBus/Decorators/DebugDecorators/DebugShowErrorMessageDecorator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaBrowser.Controller.Session;
+
+namespace SmartPlaylist.Infrastructure.MesssageBus.Decorators.DebugDecorators
+{
+    public class DebugShowErrorMessageDecorator<T> : IMessageHandlerAsync<T> where T : IMessage
+    {
+        private readonly DebugMessageManager _debugMessageManager;
+        private readonly IMessageHandlerAsync<T> _decorated;
+
+        public DebugShowErrorMessageDecorator(IMessageHandlerAsync<T> decorated,
+            ISessionManager sessionManager)
+        {
+            _decorated = decorated;
+            _debugMessageManager = new DebugMessageManager(sessionManager);
+        }
+
+        public async Task HandleAsync(T message)
+        {
+            try
+            {
+                await _decorated.HandleAsync(message).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                await _debugMessageManager.ShowAsync($"[ERROR]: {e.ToString().Substring(0,500)}").ConfigureAwait(false);
+                throw;
+            }
+        }
+    }
+}
