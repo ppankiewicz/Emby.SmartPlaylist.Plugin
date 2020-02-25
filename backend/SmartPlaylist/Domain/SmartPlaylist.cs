ee33a1dabc9583eeef700cd1df044a706b7be885
ppankiewicz <23923+ppankiewicz@users.noreply.github.com>
2020-02-25 21:53:33 +0100

    refactor: move logic for geting mediatype based on rules to MediaTypeGetter

--- backend/SmartPlaylist/Domain/SmartPlaylist.cs
+++ backend/SmartPlaylist/Domain/SmartPlaylist.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using MediaBrowser.Controller.Entities;
-using MediaBrowser.Controller.Entities.Audio;
 using MediaBrowser.Controller.Entities.TV;
 using SmartPlaylist.Comparers;
 using SmartPlaylist.Contracts;
-using SmartPlaylist.Domain.CriteriaDefinition.CriteriaDefinitions;
 using SmartPlaylist.Domain.Rule;
 using SmartPlaylist.Extensions;
 
@@ -27,7 +25,7 @@ namespace SmartPlaylist.Domain
             Limit = limit;
             LastShuffleUpdate = lastShuffleUpdate;
             UpdateType = updateType;
-            MediaType = GetDefaultMediaType(rules);
+            MediaType = MediaTypeGetter.Get(rules);
         }
 
         public Guid Id { get; }
@@ -50,27 +48,6 @@ namespace SmartPlaylist.Domain
         public bool CanUpdatePlaylistWithNewItems => (IsRandomSort || !Limit.HasLimit) && !IsShuffleUpdateType;
         public bool IsRandomSort => Limit.OrderBy is RandomLimitOrder;
         public string MediaType { get; }
-
-        private string GetDefaultMediaType(RuleBase[] rules)
-        {
-            var mediaTypes = rules.OfType<RuleGroup>()
-                .Flatten(x => x.Children.OfType<RuleGroup>())
-                .SelectMany(x => x.Children)
-                .OfType<Rule.Rule>()
-                .Select(x => x.Criteria)
-                .Where(x => x.CriteriaDefinition is MediaTypeCriteriaDefinition)
-                .Select(x => x.Value.ToString())
-                .Distinct()
-                .ToArray();
-
-            var isAudioMediaType = mediaTypes.Contains(nameof(Audio));
-            var isVideoMediaType = mediaTypes.Contains(nameof(Video)) || mediaTypes.Contains(nameof(Episode)) ||
-                                   mediaTypes.Contains(nameof(Series));
-
-            if (isVideoMediaType && !isAudioMediaType) return MediaBrowser.Model.Entities.MediaType.Video;
-
-            return MediaBrowser.Model.Entities.MediaType.Audio;
-        }
 
         private bool CheckIfCanUpdatePlaylist()
         {
