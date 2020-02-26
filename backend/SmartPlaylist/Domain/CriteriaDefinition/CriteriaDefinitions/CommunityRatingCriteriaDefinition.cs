188e665a3ae1bde0bc05c0692c01774e9d2e1b7f
ppankiewicz <23923+ppankiewicz@users.noreply.github.com>
2020-02-26 23:12:49 +0100

    feat: add CommunityRating criteria

---
+++ backend/SmartPlaylist/Domain/CriteriaDefinition/CriteriaDefinitions/CommunityRatingCriteriaDefinition.cs
@@ -0,0 +1,19 @@
+﻿using SmartPlaylist.Domain.Rule;
+using SmartPlaylist.Domain.Values;
+
+namespace SmartPlaylist.Domain.CriteriaDefinition.CriteriaDefinitions
+{
+    public class CommunityRatingCriteriaDefinition : CriteriaDefinition
+    {
+        public override string Name => "Community rating";
+        public override CriteriaDefinitionType Type => NumberDefinitionType.Instance;
+
+        public override Value GetValue(UserItem item)
+        {
+            if (item.Item.CommunityRating.HasValue)
+                return NumberValue.Create(item.Item.CommunityRating.GetValueOrDefault());
+
+            return Value.None;
+        }
+    }
+}
