6f678b57cd851a809c2b11e71d1fb29dbc94cdfc
ppankiewicz <23923+ppankiewicz@users.noreply.github.com>
2020-02-26 23:11:38 +0100

    refactor: change Number Value to float

--- backend/SmartPlaylist/Parsers/JsonValueParsers/NumberRangeValueParser.cs
+++ backend/SmartPlaylist/Parsers/JsonValueParsers/NumberRangeValueParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 using SmartPlaylist.Domain.Values;
 
 namespace SmartPlaylist.Parsers.JsonValueParsers
@@ -12,7 +13,7 @@ namespace SmartPlaylist.Parsers.JsonValueParsers
             val = null;
             var match = ParseRegEx.Match(value);
             if (match.Success && int.TryParse(match.Groups[1].Value, out var from) &&
-                int.TryParse(match.Groups[2].Value, out var to))
+                int.TryParse(match.Groups[2].Value, NumberStyles.Any, CultureInfo.InvariantCulture, out var to))
             {
                 val = NumberRangeValue.Create(from, to);
                 return true;
